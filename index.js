21a11669544b7e972ce4478139f367b3e50bc0af
magicdawn@qq.com <magicdawn@qq.com>
2016-09-25 22:36:54 +0800

    sleep: add ms lib

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 'use strict';
 
-module.exports = function promiseDelay(ms) {
+/**
+ * Module dependencies
+ */
+
+const assert = require('assert');
+const $ms = require('ms');
+
+module.exports = function sleep(ms) {
+  if (ms !== 0) {
+    assert(ms, 'the sleep duration can not be empty');
+  }
+
+  // covert
+  if (typeof ms === 'string') {
+    ms = $ms(ms);
+  }
+
   return new Promise(function(resolve) {
     setTimeout(function() {
       resolve();
--- test/simple.js
+++ test/simple.js
@@ -5,13 +5,30 @@
  */
 
 const sleep = require('../');
+const should = require('should');
 
 describe('Simple', function() {
+  it('check ms', function*() {
+    should.throws(() => {
+      sleep();
+    }, /can not be empty/);
+
+    // 0 is ok
+    yield sleep(0);
+  });
+
   it('it works', function*() {
     const ms = 10;
     const x = Date.now();
     yield sleep(ms);
     const y = Date.now();
     (y - x).should.approximately(ms, 10);
+  });
+
+  it('ms works', function*() {
+    const x = Date.now();
+    yield sleep('0.01s');
+    const y = Date.now();
+    (y - x).should.approximately(10, 10);
   });
 });
